b46bd92b1028a278366ce7aebc8821c04da904ad
jamesjcook <jamesjcook@users.noreply.github.com>
2023-04-20 19:55:57 -0400

    removed IS_Karnov from ignored assets

--- src/MWLL/Parser/Parser.php
+++ src/MWLL/Parser/Parser.php
@@ -19,7 +19,7 @@ class Parser
 	 * Vehicle types
 	 * @var array
 	 */
-	protected static $arrIgnoreVehicles = array('IS_Karnov');
+	protected static $arrIgnoreVehicles = array();
 
 	/**
 	 * Vehicles
